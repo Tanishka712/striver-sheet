4e356ea48caff31eec05f3b1a7d09a9975ed017b
Tanishka712 <taishkanagawade7@gmail.com>
2025-10-04 17:32:00 +0530

    Create pattern_problem3.cpp

---
+++ pattern_problem3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+
+    void printTriangle(int n) {
+        // code here
+        int i,j;
+        for(i=0;i<n;i++)
+        {
+            for(j=n;j>i;j--){
+            cout<<" ";}
+            for(j=0;j<2*i+1;j++)
+            {
+                cout<<"*";
+            }
+            cout<<endl;
+        }
+        
+    }
+};
